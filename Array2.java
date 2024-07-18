70822e70de5ae0939700241d7806b4bc6f0b1924
RishabhChoudhary172001 <rishabhchoudhary172001@gmail.com>
2024-07-18 13:17:40 +0530

    second commit

---
+++ Array2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Array2 {
+    public static void main(String[] args) {
+        int [] marks = new int[5];
+        marks[0] = 100;
+        marks[1] = 90;
+        marks[2] = 80;
+        marks[3] = 70;
+        marks[4] = 60;
+        for (int i =(marks.length-1);i>=0;i--){
+            System.out.println(marks[i]);
+        }
+    } 
+}
